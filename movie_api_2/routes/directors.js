dcda75e12559a4f04c121e768a0362483fab28b9
Bayram Yavuz ÇİÇEK <bayramyavuzcicek@hotmail.com>
2020-09-28 03:33:46 +0300

    api/directors/:director_id        yönetmen detay

--- movie_api_2/routes/directors.js
+++ movie_api_2/routes/directors.js
@@ -1,3 +1,4 @@
+const mongoose = require('mongoose');
 var express = require('express');
 var router = express.Router();
 
@@ -62,6 +63,64 @@ router.get('/' , (req,res)=> {
         res.json(error);
     })
 });
+
+/* id ye göre yönetmen listesi */
+router.get('/:director_id' , (req,res)=> {
+    const promise = DirectorModel.aggregate([
+        {
+            $match :  {
+                '_id' :  mongoose.Types.ObjectId(req.params.director_id)
+            } 
+        },
+        {
+            $lookup : {
+                from : 'movies',
+                localField : '_id',
+                foreignField : 'director_id',
+                as : 'movies'
+            }
+        },
+        {
+            $unwind : {
+                path : '$movies',
+                preserveNullAndEmptyArrays:  true
+            }
+        },
+        {
+            $group : {
+                _id : {
+                    _id :'$_id',
+                    name :'$name',
+                    surname: '$surname',
+                    bio : '$bio',                   
+                },
+                movies : {
+                    $push : '$movies'
+                }
+            }
+        },
+        {
+            $project:  {
+                _id : {
+                    _id : '$_id._id',
+                    name : '$_id.name',
+                    surname : '$_id.surname',
+                    bio : '$_id.bio',
+                    movies : '$movies'
+                }
+            }
+        }
+    ]);
+    promise.then((data)=> {
+        res.json(data);
+    }).catch((error)=> {
+        res.json(error);
+    })
+});
+
+
+
+
 
 
 module.exports = router;
